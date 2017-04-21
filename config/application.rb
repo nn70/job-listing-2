f05c057d7506680b98dfa0a33463199c01d1f176
NN <nn4work@gmail.com>
2017-04-21 16:11:39 +0800

    mogai base

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# config.assets.paths << "#{Rails.root}/app/assets/videos"
 
 module JobListing
   class Application < Rails::Application
