b74bcf815f3a7d0187b8437e7b73eb9fb75b29db
NN <nn4work@gmail.com>
2017-04-26 12:07:38 +0800

    fixed jimmy devise login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/alert
+// = require bootstrap/alert
 //= require bootstrap/dropdown
 //= require bootstrap/modal
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425071328) do
+ActiveRecord::Schema.define(version: 20170425154010) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170425071328) do
     t.string   "city"
     t.string   "company"
     t.string   "category"
+    t.string   "location"
   end
 
   create_table "resumes", force: :cascade do |t|
